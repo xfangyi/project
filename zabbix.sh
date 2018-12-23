b11bc9d00c431b51944d219cb643a7f0b3395d2d
xfangyi <42958742+xfangyi@users.noreply.github.com>
2018-12-23 19:19:03 +0800

    Update zabbix.sh

--- zabbix.sh
+++ zabbix.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DIR=$(pwd)
-ZABBIX="zabbix-3.4.11.tar.gz"
+ZABBIX="zabbix-3.0.24.tar.gz"
 yum -y install java-1.8.0-openjdk-devel mariadb-devel libxml2-devel pcre pcre-devle gcc gcc-c++ net-snmp-devel libevent-devel libcurl-devel \
 httpd php php-mysql  php-gettext php-session php-ctype php-xmlreader php-xmlwriter php-xml php-net-socket php-gd php-mysql php-bcmath  php-mbstring 
 
@@ -13,10 +13,10 @@ EOF
 
 
 tar xf ${DIR}/${ZABBIX} -C /opt
-cd /opt/zabbix-3.4.11 ; ./configure --prefix=/usr/local/zabbix --enable-server --enable-agent --with-mysql  --with-net-snmp --with-libcurl --with-libxml2 --enable-java 
+cd /opt/zabbix-3.0.24 ; ./configure --prefix=/usr/local/zabbix --enable-server --enable-agent --with-mysql  --with-net-snmp --with-libcurl --with-libxml2 --enable-java 
 make -j && make install 
 
-cp -r ${DIR}/zabbix-3.4.11/frontends/php/*  /var/www/html/
+cp -r ${DIR}/zabbix-3.0.24/frontends/php/*  /var/www/html/
 cp /opt/zabbix-3.4.11/misc/init.d/fedora/core/* /etc/init.d/
 
 sed -i.bak -r 's#(BASEDIR=/usr/local)#&/zabbix#g' /etc/init.d/zabbix_server
